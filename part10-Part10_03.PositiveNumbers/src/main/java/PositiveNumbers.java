c96b239cea5b0d8ed9045aef9b7c263fb71ca5c1
dias375 <dias375@gmail.com>
2023-03-13 22:04:58 -0400

    Part 14 complete

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -11,4 +11,8 @@ public class PositiveNumbers {
 
     }
 
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream().filter(s -> s > 0).collect(Collectors.toList());
+    }
+;
 }
